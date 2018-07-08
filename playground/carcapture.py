75de4b2925c17820602ef72f98d4f659f7de4347
nobinov <novianhabibie@gmail.com>
2018-07-09 01:44:34 +0200

    add everything

--- playground/carcapture.py
+++ playground/carcapture.py
@@ -11,8 +11,18 @@ import os
 import pandas as pd
 import sys
 import signal
+import image_geometry
+from pathlib import Path
 
 #rosrun image_transport republish compreed in:=output/image raw out:=camera_out/image
+
+def load_polygons(path='/home/novian/term2/dl4ad/repo2/d4dl/exercise4/polygons'):
+	p = Path(path)
+   	polygons = []
+   	for csv in [f for f in p.iterdir()]:
+   		df = pd.read_csv(csv)
+   		polygons.append(df)
+   	return polygons
  
 class CameraPose(object):
 
@@ -20,24 +30,21 @@ class CameraPose(object):
 		self.tf_listener = tf.TransformListener()
 		self.num = 0
 		self.broken_amt = 0
-
 		self.imgseq = []
 		self.timestamp = []
 		self.broken = []
-		self.x = []
-		self.y = []
-		self.z = []
-		self.w = []
+		self.mat = []
+		self.prop = None
+		self.polygon = load_polygons()
+		self.counter = 0
+
+
 
 	def callback_camerainfo(self,prop):
 		#print('got image')
-		self.gotimage = True
 		print(prop.header.stamp)
 		print(rospy.Time.now())
-		self.num=self.num+1
-		print(self.num)
-		#(trans,rot) = self.tf_listener.lookupTransform('/mocap','front_cam',prop.header.stamp)
-		#print(rot)
+		self.prop = prop
 
 	def callback_camera(self,img):
 		namefile = '{}{:06d}{}'.format('car02-frame',self.num,'.png')
@@ -53,8 +60,9 @@ class CameraPose(object):
 			#cv2.imwrite('test.jpg',image_np)
 			#print("saving car01-frame%06i.png" % self.num)
 			print("saving "+namefile)
-			cv2.imwrite(os.path.join('/home/novian/catkin_ws/src/bagfile/car-02', namefile), image_np)
-
+			#cv2.imwrite(os.path.join('/home/novian/catkin_ws/src/bagfile/car-02/', namefile), image_np)
+		mtr = []
+		trans = [0.0,0.0,0.0]	
 		rot = [0.0,0.0,0.0,0.0]
 		broken = False
 		try:
@@ -67,15 +75,43 @@ class CameraPose(object):
 			print(e)
 			broken=True
 			self.broken_amt+=1
+		else:
+			mtr = self.tf_listener.fromTranslationRotation(trans,rot)
+
+		print(mtr)
+
+		ig = image_geometry.PinholeCameraModel()
+		ig.fromCameraInfo(self.prop)
+		
+
+		out = image_np.copy()
+		for polygon in self.polygon:
+			projected = []
+			# below can probably be vectorized somehow, maybe consider using bboxes as well
+			for i, r in polygon.iterrows():
+				coords = np.array([[r.x], [r.y], [r.z], [1]])
+				projected.append(coords)
+				coords = np.matmul(np.array(mtr) ,coords)
+				coord = ig.project3dToPixel(coords)
+				print(coord)
+			projected = np.array(projected)
+			cv2.fillPoly(image_np, [projected], (0, 255, 0))
+		cv2.addWeighted(image_np, 0.7, out, 0.3, 0, out)
+		namefile = str(self.counter) + '_out.png'
+		self.counter += 1
+		cv2.imwrite(os.path.join('/home/novian/catkin_ws/src/bagfile/car-02n/', namefile), out)
 
 
-		self.imgseq.append(self.num)
+
+
+		self.mat.append(mtr)
+		self.imgseq.append(namefile)
 		self.timestamp.append(img.header.stamp)
 		self.broken.append(broken)
-		self.x.append(rot[0])
-		self.y.append(rot[1])
-		self.z.append(rot[2])
-		self.w.append(rot[3])
+		print(trans)
+		print(rot)
+		#print(self.mat)
+
 		print(self.broken_amt)
 
 	def hook(self):
@@ -84,16 +120,13 @@ class CameraPose(object):
 			'imgseq':self.imgseq,
 			'timestamp':self.timestamp,
 			'broken':self.broken,
-			'x':self.x,
-			'y':self.y,
-			'z':self.z,
-			'w':self.w}
-		df = pd.DataFrame(rawdata,columns=['imgseq','timestamp','broken','x','y','z','w'])
-		df.to_csv('car-02.csv')
+			'mat':self.mat}
+		df = pd.DataFrame(rawdata,columns=['imgseq','timestamp','broken','mat'])
+		#df.to_csv('car-01.csv')
 
 	def listener(self):
 		rospy.init_node('listener',anonymous=True)
-		#rospy.Subscriber("output/camera_info", CameraInfo, self.callback_camerainfo)
+		rospy.Subscriber("output/camera_info", CameraInfo, self.callback_camerainfo)
 		rospy.Subscriber("output/image/compressed", CompressedImage, self.callback_camera)
 		rospy.on_shutdown(self.hook)
 		rospy.spin()
